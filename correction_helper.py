c94caf054078b24eb0306d616f4969745b89e44c
Julien Palard <julien@palard.fr>
2021-05-23 10:00:44 +0200

    Adding a tox.ini with flake8, mypy, pylint.

--- correction_helper.py
+++ correction_helper.py
@@ -1,17 +1,18 @@
-from io import StringIO
-import random
-from pathlib import Path
-import sys
+"""Set of tools to help writing correction bots in Python for Python."""
 import gettext
 import os
-import signal
-from itertools import zip_longest
-from traceback import format_exc
-from contextlib import redirect_stdout, redirect_stderr
-from textwrap import indent
-from contextlib import contextmanager
-import subprocess
+import random
 import shlex
+import signal
+import subprocess
+import sys
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from dataclasses import dataclass
+from io import StringIO
+from itertools import zip_longest
+from pathlib import Path
+from textwrap import indent
+from traceback import format_exc
 
 import friendly_traceback
 from friendly_traceback import exclude_file_from_traceback
@@ -26,10 +27,12 @@ _ = gettext.translation("check", Path(__file__).parent, fallback=True).gettext
 
 
 def code(text, language="text"):
+    """Transform given text as a Markdown code block."""
     return "    :::" + language + "\n" + indent(str(text), "    ")
 
 
-def stderr(*args, **kwargs):
+def print_stderr(*args, **kwargs):
+    """`print` wrapper outputing to stderr."""
     print(*args, **kwargs, file=sys.stderr)
 
 
@@ -39,12 +42,12 @@ def fail(*args, sep="\n\n"):
     By default, if multiple args are given, they are separated by two
     newlines, usefull to build Markdown paragraphs.
     """
-    stderr(sep.join(args))
+    print_stderr(sep.join(args))
     sys.exit(1)
 
 
 def congrats():
-    """Generates a generic congratulation sentence."""
+    """Generate a generic congratulation sentence."""
     return random.choice(
         [
             "Congrats! Your exercise is OK.",
@@ -57,13 +60,18 @@ def congrats():
     )
 
 
-def _handle_student_exception(prefix, friendly=False):
+def _handle_student_exception(prefix=None, friendly=False):
+    """Handle a student exception.
+
+    Can preprend an optional prefix, and can use friendly to display
+    friendly explanation.
+    """
     if friendly:
         friendly_traceback.explain_traceback()
     else:
         if prefix:
-            stderr(prefix, end="\n\n")
-        stderr(code("".join(format_exc()), "pytb"))
+            print_stderr(prefix, end="\n\n")
+        print_stderr(code("".join(format_exc()), "pytb"))
     sys.exit(1)
 
 
@@ -71,34 +79,39 @@ class Tee(StringIO):
     """Like tee. (man tee), combining a StringIO and a normal file."""
 
     def __init__(self, twin, **kwargs):
+        """Build this StringIO with a twin StringIO attached to it."""
         self.twin = twin
         super().__init__(**kwargs)
 
     def write(self, s):
+        """Write to this StringIO and its twin."""
         self.twin.write(indent(s, "    "))
         super().write(s)
 
 
 class Run:
-    def __init__(self, stdout, stderr):
+    """Representation for a program or function run storing stdout and stderr."""
+
+    def __init__(self, stdout: StringIO, stderr: StringIO):
+        """Create a Run instance from two StringIO (or Twin instances)."""
         self.stdout = stdout
         self.stderr = stderr
 
     @property
     def out(self):
+        """Stdout, stripped."""
         return self.stdout.getvalue().strip()
 
     @property
     def err(self):
+        """Stderr, stripped."""
         return self.stderr.getvalue().strip()
-
-
-class TimeoutError(Exception):
-    pass
 
 
 @contextmanager
 def deadline(timeout=1):
+    """Context manager raising a TimeoutError after a given deadline in seconds."""
+
     def handler(signum, frame):
         raise TimeoutError
 
@@ -109,7 +122,7 @@ def deadline(timeout=1):
 
 
 @contextmanager
-def student_code(
+def student_code(  # pylint: disable=too-many-arguments,too-many-branches
     exception_prefix="Got an exception:",
     friendly=True,
     print_allowed=False,
@@ -120,7 +133,7 @@ so maybe just replace your `print` call by a `return` statement.""",
     too_slow_message="Your program looks too slow, looks like an infinite loop.",
     timeout=1,
 ):
-    """Decorator usefull to run student code.
+    """Execute student code under surveillance.
 
     It help them spot common errors like:
     - using `input()`
@@ -140,15 +153,15 @@ so maybe just replace your `print` call by a `return` statement.""",
     """
     old_stdin = sys.stdin
     if print_allowed is None:
-        run = Run(Tee(sys.stdout), Tee(sys.stderr))
+        capture = Run(Tee(sys.stdout), Tee(sys.stderr))
     else:
-        run = Run(StringIO(), StringIO())
+        capture = Run(StringIO(), StringIO())
     try:
         sys.stdin = None
-        with redirect_stdout(run.stdout):
-            with redirect_stderr(run.stderr):
+        with redirect_stdout(capture.stdout):
+            with redirect_stderr(capture.stderr):
                 with deadline(timeout):
-                    yield run
+                    yield capture
     except TimeoutError:
         fail(too_slow_message)
     except SystemExit:
@@ -161,28 +174,30 @@ else I won't be able to check it."""
         if "lost sys.stdin" not in str(err):
             _handle_student_exception(exception_prefix, friendly)
         fail("Don't use the `input` builtin, there's no human to interact with here.")
-    except:  # noqa
+    except:  # noqa  pylint: disable=bare-except
         _handle_student_exception(exception_prefix, friendly)
     finally:
         sys.stdin = old_stdin
     if print_allowed is False:
-        if run.err or run.out:
-            stderr(print_prefix, end="\n\n")
-            if run.err:
-                stderr(code(run.err, language="text"))
-            if run.out:
-                stderr(code(run.out, language="text"))
-            if print_expect and run.out == print_expect:
-                stderr(print_expect_message, end="\n\n")
+        if capture.err or capture.out:
+            print_stderr(print_prefix, end="\n\n")
+            if capture.err:
+                print_stderr(code(capture.err, language="text"))
+            if capture.out:
+                print_stderr(code(capture.out, language="text"))
+            if print_expect and capture.out == print_expect:
+                print_stderr(print_expect_message, end="\n\n")
             sys.exit(1)
 
 
+@dataclass
 class Section:
-    def __init__(self, name, prefix="", suffix="", highlight=""):
-        self.name = name
-        self.prefix = prefix
-        self.suffix = suffix
-        self.highlight = highlight
+    """A section of friendly output."""
+
+    name: str
+    prefix: str = ""
+    suffix: str = ""
+    highlight: str = ""
 
 
 MARKDOWN_ITEMS = [
@@ -205,7 +220,9 @@ MARKDOWN_ITEMS = [
 ]
 
 
-def friendly_traceback_markdown(info, level=None, include=None):
+def friendly_traceback_markdown(
+    info, level=None, include=None
+):  # pylint: disable=unused-argument
     """Traceback formatted with full information but with markdown syntax."""
     result = []
 
@@ -227,7 +244,8 @@ def friendly_traceback_markdown(info, level=None, include=None):
 friendly_traceback.set_formatter(friendly_traceback_markdown)
 
 
-def run(file, *args):
+def run(file, *args):  # pylint: disable=too-many-branches
+    """subprocess.run wrapper specialized to run Python with friendly."""
     start_hint = ""
     if args:
         args = ["--"] + list(args)
@@ -260,7 +278,10 @@ def run(file, *args):
                 stdout = "Your code printed:\n\n" + code(err.stdout)
         if err.stderr:
             if len(err.stderr) > 1_000:
-                stdout = f"Your code printed {len(err.stderr)} characters on stderr before being interrupted."
+                stdout = (
+                    f"Your code printed {len(err.stderr)} "
+                    "characters on stderr before being interrupted."
+                )
             else:
                 stdout = "Found this on stderr:\n\n" + code(err.stderr)
         if err.returncode == -9:
@@ -304,72 +325,73 @@ def code_or_repr(some_string):
     """
     if len(some_string) < 10 and "`" not in some_string:
         return f" `{some_string}`"
-    else:
-        return "\n\n" + code(some_string)
+    return "\n\n" + code(some_string)
 
 
-def compare(my, theirs, preamble=""):
-    """Compare two results: mine (expected to be the right one) and theirs
-    (the student output).
+def compare(expected, theirs, preamble=""):
+    """Compare two results.
+
+    expected (the right one) and theirs (student output).
 
     Both can be multi-lines, and if they differ, a proper message will
     be raised, using correction_helper.fail.
-
     """
-    if my.strip() == theirs.strip():
+    if expected.strip() == theirs.strip():
         return
-    for line, (m, t) in enumerate(
-        zip_longest(my.split("\n"), theirs.split("\n")), start=1
+    for line, (expected_line, their_line) in enumerate(
+        zip_longest(expected.split("\n"), theirs.split("\n")), start=1
     ):
-        if m != t:
-            if m is None:
+        if expected_line != their_line:
+            if expected_line is None:
                 fail(
                     preamble,
                     _("Unexpected line {line}, you gave:").format(line=line)
-                    + code_or_repr(t),
+                    + code_or_repr(their_line),
                     _("Just in case it helps, here's your full output:"),
                     code(theirs),
                 )
-            elif t is None:
+            elif their_line is None:
                 fail(
                     preamble,
                     _(
                         "Your output is too short, missing line {line}, I'm expecting:"
                     ).format(line=line)
-                    + code_or_repr(m),
+                    + code_or_repr(expected_line),
                     _("Just in case it helps, here's your full output:"),
                     code(theirs),
                 )
             else:
                 hint = ""
                 trailer = ""
-                if t != theirs:
+                if their_line != theirs:
                     trailer = (
                         _("Just in case it helps, here's your full output:")
                         + "\n\n"
                         + code(theirs)
                     )
-                if m and t:
-                    if t[0] == " " and m[0] != " ":
+                if expected_line and their_line:
+                    if their_line[0] == " " and expected_line[0] != " ":
                         hint = (
                             "\n\n" + "(Notice your line starts with a space, not mine.)"
                         )
-                    if t[-1] == " " and m[-1] != " ":
+                    if their_line[-1] == " " and expected_line[-1] != " ":
                         hint = (
                             "\n\n" + "(Notice your line ends with a space, not mine.)"
                         )
                 fail(
                     preamble,
                     _("On line {line} I'm expecting:").format(line=line)
-                    + code_or_repr(m),
-                    (_("You gave:") + code_or_repr(t)) if t else _("You gave nothing."),
+                    + code_or_repr(expected_line),
+                    (_("You gave:") + code_or_repr(their_line))
+                    if their_line
+                    else _("You gave nothing."),
                     hint,
                     trailer,
                 )
     fail(
         preamble,
         "Looks like a wrong answer, expected:",
-        code(my),
+        code(expected),
         "you gave:",
         code(theirs),
     )
