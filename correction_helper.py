ec68ae8a5c202d44c40f1d18f4d860d51ac46ff5
Julien Palard <julien@palard.fr>
2023-05-25 10:27:58 +0200

    Releasing v2023.5

--- correction_helper.py
+++ correction_helper.py
@@ -18,7 +18,7 @@ from typing import Optional, Sequence, Tuple, Union
 import friendly_traceback
 from friendly_traceback import exclude_file_from_traceback
 
-__version__ = "2022.9.16"
+__version__ = "2023.5"
 
 friendly_traceback.set_lang(os.environ.get("LANGUAGE", "en"))
 
